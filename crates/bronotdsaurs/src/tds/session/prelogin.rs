9c6584b5267b405fc3233cd5390fd14d5c73344f
Jason Ng <7614175+jsnng@users.noreply.github.com>
2026-04-28 20:06:03 +1000

    fix: TLS async-ugly-ification

--- crates/bronotdsaurs/src/tds/session/prelogin.rs
+++ crates/bronotdsaurs/src/tds/session/prelogin.rs
@@ -71,16 +71,14 @@ pub enum InitialStateTransition<T, O> {
 }
 
 #[cfg(not(feature = "tds8.0"))]
-impl<T: AsyncTransport, O: Observer<Event>> Session<InitialState, T, O> {
+impl<T: AsyncTransport + Transport, O: Observer<Event>> Session<InitialState, T, O> {
     pub async fn transition(
         mut self,
         prelogin: PreLoginPacket,
     ) -> Result<InitialStateTransition<T, O>, SessionError> {
-        self.stream
-            .set_read_timeout(self.timers.connection)
+        AsyncTransport::set_read_timeout(&mut self.stream, self.timers.connection)
             .map_err(|_| SessionError::transport_read_error())?;
-        self.stream
-            .set_write_timeout(self.timers.connection)
+        AsyncTransport::set_write_timeout(&mut self.stream, self.timers.connection)
             .map_err(|_| SessionError::transport_write_error())?;
         self.send(prelogin).await?;
         self.notify(Event::PreLoginSent);
@@ -146,7 +144,7 @@ impl<T: AsyncTransport, O: Observer<Event>> Session<InitialState, T, O> {
 }
 
 #[cfg(feature = "tls")]
-impl<T: Transport, O: Observer<Event>> Session<TlsSslNegotiationState, T, O> {
+impl<T: AsyncTransport + Transport, O: Observer<Event>> Session<TlsSslNegotiationState, T, O> {
     pub async fn transition<P, H, F>(
         self,
         server_name: &str,
@@ -154,10 +152,10 @@ impl<T: Transport, O: Observer<Event>> Session<TlsSslNegotiationState, T, O> {
         factory: F,
     ) -> Result<Session<LoginReadyState, P, O>, SessionError>
     where
-        P: Transport,
+        P: AsyncTransport,
         H: TlsHandshaker,
         H::HandshakeError: core::fmt::Debug,
-        T::Error: core::fmt::Debug,
+        <T as Transport>::Error: core::fmt::Debug,
         F: FnOnce(T, H::Connection) -> P,
     {
         let Session {
